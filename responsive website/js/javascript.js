df79365ed829679ce460b065b4ad041b0859e2c4
Urvashi146 <aidev.urvashi@gmail.com>
2023-04-06 15:08:04 +0530

    added javascript file on responsive  website folder and also added event.js sample file

---
+++ responsive website/js/javascript.js
@@ -0,0 +1,14 @@
+
+        function toggle() {
+            let pra = document.getElementById('pra');
+            let prabtn = document.getElementById('prabtn');
+
+            if (pra.style.display == 'none')
+             {
+                pra.style.display = 'block';
+            }
+            else {
+                pra.style.display = 'none';
+            }
+        }
+    
