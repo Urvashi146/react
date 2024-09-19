70ea4ef60c29a2dfff3a0b74d37f76b432183e73
Urvashi146 <aidev.urvashi@gmail.com>
2024-09-19 11:56:39 +0530

    demos

---
+++ react/ReactTutorial/demos/src/App.js
@@ -0,0 +1,9 @@
+import logo from './logo.svg';
+import './App.css';
+import Firstfun from "./Components/FunctionComponent"
+
+function App() {
+  return (<><Firstfun/></>)
+}
+
+export default App;
---
+++ react/ReactTutorial/demos/src/Components/FunctionComponent.js
@@ -0,0 +1,15 @@
+function Firstfun (){
+    return <>
+    
+
+    <h1 className="text-center mt-5 text-decoration-underline ">
+        Hello, Welcome To React JS.</h1>
+
+        </>
+}
+
+export default Firstfun;
+
+// this is another way to declare class and export in one line . and in app.js file we need to import like this =>import {Firstfun} from "./Components/FunctionComponent
+
+// export const Firstfun = () =>  <h1 className="text-center">Hello, Welcome To React JS.</h1>
